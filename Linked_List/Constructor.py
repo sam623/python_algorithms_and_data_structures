0f3ca28d0c525539efe49b31fb962e33fb0ea95b
sam623 <rahimi.saman@yahoo.com>
2022-01-03 20:21:11 -0800

    Update Constructor.py

--- Linked_List/Constructor.py
+++ Linked_List/Constructor.py
@@ -1,1 +1,16 @@
 
+# Node class that creates individual nodes
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+    
+    
+# Linked List Class
+class LinkedList:
+  def __init__(self,value):
+    new_node = Node(value)
+    self.head = new_node
+    self.tail = new_node
+    self.length = 1
+  
