62bb589333526f98c028b858c45c1d2b75b403fa
sam623 <rahimi.saman@yahoo.com>
2022-01-04 21:57:15 -0800

    Update linked_list.py

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -1,16 +1,23 @@
-
 # Node class that creates individual nodes
 class Node:
-  def __init__(self, value):
-    self.value = value
+  def __init__(self, data):
+    self.data = data
     self.next = None
     
     
 # Linked List Class
 class LinkedList:
-  def __init__(self,value):
-    new_node = Node(value)
+  def __init__(self,data):
+    new_node = Node(data)
     self.head = new_node
     self.tail = new_node
     self.length = 1
+
+# Print Linked List
+  def print_Linked_List(self):
+    temp = self.head
+    while temp is not None:
+      print(temp.data)
+      temp = temp.next
+
   
