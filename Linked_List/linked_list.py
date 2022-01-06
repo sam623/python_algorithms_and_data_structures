f6c1f97f6b4c94bbdd527d17dbfd2d57126df44c
sam623 <rahimi.saman@yahoo.com>
2022-01-05 21:27:19 -0800

    Update linked_list.py

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -47,4 +47,25 @@ class LinkedList:
             self.tail.next = None
             self.length -= 1    
         return self.tail.data
-  
+    
+    def prepend(self,data):
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += self.length
+        return True
+    def pop_first(self):
+        if self.head.next is None:
+            self.head = None
+            self. tail = None
+            self.length = 0
+        else:
+            temp = self.head
+            self.head = temp.next
+            temp.next = None
+            self.length -+ self.length
+        return True 
